d4f1921f4901ccb1b9b018250c048dfeda891e6b
melonique <melonique@gmail.com>
2024-07-19 18:59:01 +0000

    responsive

--- components/bingo-sheet.tsx
+++ components/bingo-sheet.tsx
@@ -35,38 +35,40 @@ export function BingoSheet() {
       <h2 className="text-2xl font-bold mb-4">Bingo Habitus</h2>
 
       <p>En circulant dans notre magnifique cour estivale, trouve quelqu’un qui…</p>
-      <div className="grid grid-cols-4 m-2 border">
-        {[
-          "Joue d’un instrument de musique",
-          "Habite ici depuis juin 2017",
-          "Est né au mois de mars",
-          "Possède un autre animal qu’un chat ou un chien",
-          "A déjà publié un livre",
-          "A de fortes allergies au point de posséder un auto-éjecteur d’adrénaline (EpiPen)",
-          "Est impliqué dans un comité Habitus (social, déneigement, jardin, etc.)",
-          "A 3 enfants ou plus",
-          "Porte du jaune aujourd’hui",
-          "N’aime pas les sushis",
-          "N’est pas propriétaire d’une voiture",
-          "S’est déjà fait voler plus de 1000$ depuis qu’il.elle habite ici.",
-          "Est le.la président.e du CA d’Habitus",
-          "N’a pas de climatiseur",
-          "A un nid d’oiseau à – de 20 mètres de sa résidence",
-          "N’est pas allé.e voter aux dernières élections provinciales",
-          "Fait ou a déjà fait du sport de compétition de haut niveau",
-          "Possède un Doctorat",
-          "Parle une autre langue que le français ou l’anglais",
-          "A plus de 20 plantes dans sa maison",
-        ].map((text, index) => (
-          <button
-            key={index}
-            disabled={!loaded}
-            className={`bg-card p-4 rounded-md border ${toggledItems.has(index) ? "bg-primary text-primary-foreground" : ""}`}
-            onClick={() => toggleItem(index)}
-          >
-            <p>{text}</p>
-          </button>
-        ))}
+      <div className="overflow-scroll w-full">
+        <div className="grid grid-cols-4 m-2 border w-[600px]">
+          {[
+            "Joue d’un instrument de musique",
+            "Habite ici depuis juin 2017",
+            "Est né au mois de mars",
+            "Possède un autre animal qu’un chat ou un chien",
+            "A déjà publié un livre",
+            "A de fortes allergies au point de posséder un auto-éjecteur d’adrénaline (EpiPen)",
+            "Est impliqué dans un comité Habitus (social, déneigement, jardin, etc.)",
+            "A 3 enfants ou plus",
+            "Porte du jaune aujourd’hui",
+            "N’aime pas les sushis",
+            "N’est pas propriétaire d’une voiture",
+            "S’est déjà fait voler plus de 1000$ depuis qu’il.elle habite ici.",
+            "Est le.la président.e du CA d’Habitus",
+            "N’a pas de climatiseur",
+            "A un nid d’oiseau à – de 20 mètres de sa résidence",
+            "N’est pas allé.e voter aux dernières élections provinciales",
+            "Fait ou a déjà fait du sport de compétition de haut niveau",
+            "Possède un Doctorat",
+            "Parle une autre langue que le français ou l’anglais",
+            "A plus de 20 plantes dans sa maison",
+          ].map((text, index) => (
+            <button
+              key={index}
+              disabled={!loaded}
+              className={`bg-card p-4 rounded-md border ${toggledItems.has(index) ? "bg-primary text-primary-foreground" : ""}`}
+              onClick={() => toggleItem(index)}
+            >
+              <p>{text}</p>
+            </button>
+          ))}
+        </div>
       </div>
       <p className="mt-4">La personne qui remporte ce BINGO est celle ayant le plus de bonnes réponses (un petit prix sera offert).</p>
       <p className="mt-2">Bonne recherche! Le comité social Habitus.</p>
